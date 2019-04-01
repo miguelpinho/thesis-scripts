88986174f2159f021a87040e81250cb50427fb22
Miguel Pinho <miguel.m.pinho@tecnico.ulisboa.pt>
2019-04-01 23:06:40 +0100

    Add gem5 arm full-system base scripts.

---
+++ gem5-fs/arm_fs.sh
@@ -0,0 +1,11 @@
+export M5_PATH=/home/miguelp/gem5-full-system/aarch-system-20180409
+GEM5_PATH=/home/miguelp/gem5
+
+$GEM5_PATH/build/ARM/gem5.opt \
+    -d /home/miguelp/Documents/Blue/prototype/fs-logs/trusty-O3CPU \
+    $GEM5_PATH/configs/example/fs.py \
+    --machine-type=VExpress_GEM5_V1 \
+    --kernel=$M5_PATH/binaries/vmlinux.vexpress_gem5_v1_64 \
+    --disk-image=$M5_PATH/disks/aarch64-ubuntu-trusty-headless.img \
+    --dtb-filename=$M5_PATH/binaries/armv8_gem5_v1_1cpu.dtb \
+    --caches --l2cache
