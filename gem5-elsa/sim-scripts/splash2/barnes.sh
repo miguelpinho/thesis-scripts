d9e0e9192ad8a955e3331b8155bb4b4b8097b424
Miguel Pinho <miguel.m.pinho@tecnico.ulisboa.pt>
2019-04-03 00:14:55 +0100

    Fix stupid merge.

--- gem5-elsa/sim-scripts/splash2/barnes.sh
+++ gem5-elsa/sim-scripts/splash2/barnes.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
 m5 dumpresetstats
-<<<<<<< HEAD
 ./home/splash2/codes/apps/barnes/BARNES < ./home/splash2/codes/apps/barnes/input
-=======
-./home/splash2/codes/apps/fmm/BARNES < ./home/splash2/codes/apps/fmm/input
->>>>>>> 90e93fde2ae2746126a16dddcc727267d429e2ea
 m5 dumpresetstats
 m5 exit
