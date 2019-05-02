ef9d3025dddeea4b2f7ffe21827be0ead2a1751b
Miguel Pinho <miguel.m.pinho@tecnico.ulisboa.pt>
2019-05-02 13:41:04 +0100

    Change spec2006 folder.

--- sim-scripts/spec2006/hmmer.sh
+++ sim-scripts/spec2006/hmmer.sh
@@ -6,7 +6,7 @@ pwd
 ls
 
 m5 dumpresetstats
-./benchspec/CPU2006/456.hmmer/exe/hmmer_base.arm-64bit \
+./benchspec/CPU2006/456.hmmer/exe/hmmer_base.gcc83-aarch64 \
     ./benchspec/CPU2006/456.hmmer/data/train/input/leng100.hmm
 m5 dumpresetstats
 m5 exit
--- sim-scripts/spec2006/libquantum.sh
+++ sim-scripts/spec2006/libquantum.sh
@@ -6,6 +6,7 @@ pwd
 ls
 
 m5 dumpresetstats
-./benchspec/CPU2006/462.libquantum/exe/libquantum_base.arm-64bit 1397 8
+./benchspec/CPU2006/462.libquantum/exe/libquantum_base.gcc83-aarch64 \
+    1397 8
 m5 dumpresetstats
 m5 exit
