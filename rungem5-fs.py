bb267a586e3c29aa81b0c02a29580922b833af79
Miguel Pinho <miguel.m.pinho@tecnico.ulisboa.pt>
2019-06-11 01:47:40 +0100

    First successful rungem5 script run.

--- rungem5-fs.py
+++ rungem5-fs.py
@@ -6,9 +6,10 @@ Util and module for running gem5 full-system simulations.
 """
 
 import argparse
-import datetime
+from datetime import datetime
 import subprocess
 import os
+import sys
 from pathlib import Path
 from dotenv import load_dotenv
 
@@ -17,50 +18,104 @@ def get_arguments():
     """Parse rungem5 arguments."""
     parser = argparse.ArgumentParser(description='Run gem5 full-system.')
     subparsers = parser.add_subparsers(
-        title='action', help='action to perform')
+        title='action',
+        dest='action',
+        help='action to perform'
+    )
+    subparsers.required = True
 
     # generic options
     # env
     parser.add_argument(
         '--env-file',
         default=argparse.SUPPRESS,
-        help='''Script file frow where to load environment variables.'''
+        help='''script file frow where to load environment variables'''
     )
     parser.add_argument(
         '--gem5-path',
         default=argparse.SUPPRESS,
-        help='''Path to the gem5 main directory. Overrides environment
-        variable.'''
+        help='''path to the gem5 main directory. Overrides environment
+        variable'''
     )
     parser.add_argument(
         '--m5-path',
         default=argparse.SUPPRESS,
-        help='''Path to the gem5 full-system files. Overrides environment
-        variable.'''
+        help='''path to the gem5 full-system files. Overrides environment
+        variable'''
     )
     parser.add_argument(
         '--gem5-ckpoint-path',
         default=argparse.SUPPRESS,
-        help='''Root directory for execution checkpoints. Overrides
-        environment variable.'''
+        help='''root directory for execution checkpoints. Overrides
+        environment variable'''
     )
     parser.add_argument(
         '--m5out-path',
         default=argparse.SUPPRESS,
-        help='''Root directory for the generated output. Overrides
-        environment variable.'''
+        help='''root directory for the generated output. Overrides
+        environment variable'''
+    )
+    # gem5 options
+    parser.add_argument(
+        '--arch',
+        choices=('ARM', 'X86', 'ALPHA', 'SPARC', 'RISCV'),
+        default='ARM',
+        help='''cpu architecture'''
     )
     parser.add_argument(
+        '--binary-mode',
+        choices=('debug', 'opt', 'fast', 'prof', 'perf'),
+        default='opt',
+        help='''gem5 binary mode'''
+    )
+    parser.add_argument(
+        '--out-folder',
+        default=argparse.SUPPRESS,
+        help='''m5out sub-folder name. Overrides default folder for action''',
+    )
+    # simulation options
+    parser.add_argument(
         '-N',
-        '--num-jobs',
+        '--sim-jobs',
+        type=int,
         default=4,
-        help='''Max number of simulation jobs allowed to be running in
-        parallel. Overrides environment variable.''',
+        help='''max number of simulation jobs allowed to be running in
+        parallel. Overrides environment variable''',
     )
     parser.add_argument(
         '--benchmarks-path',
-        help='''Root directory for the benchmarks. Overrides environment
-        variable.'''
+        help='''root directory for the benchmarks. Overrides environment
+        variable'''
+    )
+    parser.add_argument(
+        '-C',
+        '--cpu-cores',
+        type=int,
+        default=1,
+        help='''number of cores in each simulated cpu'''
+    )
+    parser.add_argument(
+        '--no-caches',
+        action="store_true",
+        default=False,
+        help='''number of cores in each simulated cpu'''
+    )
+    parser.add_argument(
+        '--fast-cpu',
+        action="store_true",
+        default=False,
+        help='''use AtomicSimpleCPU as the main cpu-type'''
+    )
+
+    # "info" action
+    parser_info = subparsers.add_parser(
+        'info', help='show the gem5 or config file help')
+    parser_info.add_argument(
+        'info-option',
+        choices=('gem5', 'config'),
+        default='gem5',
+        nargs='?',
+        help='info to show'
     )
 
     # "boot" action
@@ -77,10 +132,26 @@ def get_arguments():
     # "script" action
     parser_script = subparsers.add_parser(
         'script', help='restore and run script')
+    parser_script.add_argument(
+        '--script',
+        default=argparse.SUPPRESS,
+        help='''script to run on boot'''
+    )
 
     # "benchmark" action
     parser_benchmark = subparsers.add_parser(
-        'benchmark', help='restore and run available benchmarks')
+        'benchmark', help='restore and run chosen benchmarks')
+    parser_benchmark.add_argument(
+        'bench',
+        choices=('all', 'spec2006', 'parsec3',
+                 'splash2', 'apps', 'micro-kernels'),
+        help='benchmark suite to run'
+    )
+    parser_benchmark.add_argument(
+        '--workload',
+        choices=('tiny', 'small', 'large'),
+        help='size of the benchmarks workload'
+    )
 
     return parser.parse_args()
 
@@ -117,7 +188,7 @@ def get_paths(args):
     """
     paths = {}
 
-    # description | arg | env | path
+    # (description, arg, env, path)
     req_paths = [
         ("gem5 root dir", 'gem5_path', 'GEM5_PATH', 'GEM5'),
         ("full-system files dir", 'm5_path', 'M5_PATH', 'M5'),
@@ -130,25 +201,126 @@ def get_paths(args):
 
     for p in req_paths:
         if p[1] in args:
-            paths[p[3]] = dic_args[p[1]]
+            path = Path(dic_args[p[1]])
         elif p[2] in os.environ:
-            paths[p[3]] = os.environ[p[2]]
+            path = Path(os.environ[p[2]])
         else:
-            print("No path for '{}' was provided. Use option or env "
-                  "variable '{}'.".format(p[0], p[2]))
-            pass
+            # TODO: add option name
+            print("No path for '{}' was provided. Use option '--{}' or env "
+                  "variable '{}'.".format(p[0], p[1], p[2]))
+            sys.exit()
+
+        if not path.exists() or not path.is_dir():
+            print("Invalid path for '{}': '{}'.".format(p[0], path))
+            sys.exit()
+        paths[p[3]] = path
 
     return paths
 
 
-def run_fs(paths, args_gem5, args_config):
+def get_folder_path(args, paths):
+    """Return the tag for the output folder tree."""
+    date_tag = datetime.utcnow().strftime("%d-%b-%Y").lower()
+
+    output_path = Path(paths['M5OUT']) / date_tag
+    return output_path
+
+
+def get_run_tag(args):
+    """Returns the tag for this run's output."""
+    if args.action == "info":
+        return ""
+    elif "out_folder" in args:
+        return args.out_folder
+
+    # Number of cpus simulated
+    cpus = args.cpu_cores
+    cpus_tag = "{}cpu".format(cpus)
+    if cpus != 1:
+        cpus_tag += "s"
+
+    # Name of the action performed
+    # If it was a benchmark, include its name and size
+    if args.action == "benchmark":
+        task_tag = args.bench + '-' + args.workload
+    else:
+        task_tag = args.action
+
+    # Timestamp
+    time_tag = datetime.utcnow().strftime("%Hh%Mm")
+
+    return task_tag + '-' + cpus_tag + '_' + time_tag
+
+
+def get_gem5_bin(args, paths):
+    bin_name = "gem5.{}".format(args.binary_mode)
+    arch = args.arch
+
+    bin_path = paths['GEM5'] / 'build' / arch / bin_name
+
+    if not bin_path.exists():
+        print("Invalid gem5 binary path: '{}'.".format(bin_path))
+        sys.exit()
+
+    return bin_path
+
+
+def get_gem5_args(args, paths):
+    """Returns the list of args to be passed to the gem5 binary."""
+    args_gem5 = ['--redirect-stdout', '--redirect-stderr']
+
+    out_path = get_folder_path(args, paths) / get_run_tag(args)
+    args_gem5.append("--outdir={}".format(out_path))
+
+    return args_gem5
+
+
+def get_config_args(args, paths):
+    """Returns the list of args to be passed to the config script."""
+    # TODO: Fix how paths are generated (Path). Add disk image path option
+    args_config = []
+
+    num_cpus = args.cpu_cores
+    arch = args.arch
+
+    args_config.append('--num-cpus={}'.format(num_cpus))
+
+    # full-system files
+    if arch == 'ARM':
+        # architecure specific files
+        args_config.append(
+            '--disk-image={}/disks/benchmarks.img'.format(paths['M5']))
+
+        args_config.append('--machine-type=VExpress_GEM5_V1')
+        args_config.append(
+            '--kernel={}/binaries/vmlinux.vexpress_gem5_v1_64'.format(paths['M5']))
+
+        if num_cpus == 1:
+            # number of cpus specific config
+            args_config.append(
+                '--dtb-filename={}/binaries/armv8_gem5_v1_1cpu.dtb'.format(paths['M5']))
+        else:
+            print("Unsupported number of simulation cpus: '{}'.".format(num_cpus))
+            sys.exit()
+    else:
+        print("Unsupported architecture: '{}'.".format(arch))
+        sys.exit()
+
+    # caches
+    if args.no_caches == False:
+        args_config.extend(['--caches', '--l2cache'])
+
+    return args_config
+
+
+def run_fs(paths, bin_gem5, args_gem5, config_script, args_config):
     """Run gem5 with the given arguments."""
-    args = [paths['GEM5']] + args_gem5 + \
-        [paths['GEM5'] + '/config/fs.py'] + args_config
+    args = [str(bin_gem5)] + args_gem5 + \
+        [str(config_script)] + args_config
 
-    print('Running command: {}'.format(' '.join(args)))
+    print('Running command:\n{}'.format(' \\\n'.join(args)))
 
-    # subprocess.run()
+    subprocess.run(args)
 
 
 def main():
@@ -158,11 +330,12 @@ def main():
     paths = get_paths(args)
 
     print(paths)
+    bin_gem5 = get_gem5_bin(args, paths)
+    args_gem5 = get_gem5_args(args, paths)
+    config_script = paths['GEM5'] / 'configs' / 'example' / 'fs.py'
+    args_config = get_config_args(args, paths)
 
-    args_gem5 = []
-    args_config = []
-
-    run_fs(paths, args_gem5, args_config)
+    run_fs(paths, bin_gem5, args_gem5, config_script, args_config)
 
 
 if __name__ == "__main__":
