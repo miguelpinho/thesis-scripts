d5b7b2d764536c267d6ed6dee1ecd40e88925c52
Miguel Pinho <miguel.m.pinho@tecnico.ulisboa.pt>
2019-06-23 16:41:46 +0100

    Add arguments to analyse_width util.

--- util/analyse_width.py
+++ util/analyse_width.py
@@ -11,6 +11,7 @@ Util and module for analysing the width distribution of a data set.
 
 
 def get_signed_width(val):
+    """Return the signed bit-width of an integer value."""
     val = int(val)
     if val < 0:
         return val.bit_length()
@@ -19,14 +20,65 @@ def get_signed_width(val):
 
 
 def get_unsigned_width(val):
+    """Return the unsigned bit-width of an integer value."""
     val = int(val)
     return val.bit_length()
 
 
-def main():
-    data = np.genfromtxt('normal_mu10_s10_n100_seed777733.csv', dtype=int)
+def get_data_width(data, width_def='signed'):
+    option = width_def.lower().strip()
 
-    width = np.array(list(map(get_signed_width, data)))
+    if option == 'signed':
+        width_func = get_signed_width
+    elif option == 'unsigned':
+        width_func = get_unsigned_width
+    else:
+        print("Invalid width definition: {}".format(width_def))
+
+    return np.array(list(map(width_func, data)))
+
+
+def read_data(file):
+    """Return data read from a .csv formatted file."""
+    path = Path(file)
+    if not path.is_file():
+        print('Invalid file path: {}'.format(path))
+        sys.exit()
+
+    return np.genfromtxt(str(path), dtype=int)
+
+
+def get_args():
+    """Parse analyse_width util arguments."""
+    parser = argparse.ArgumentParser(description='''Analyse the width
+    distribution of an integer data set''')
+
+    parser.add_argument(
+        '--bits',
+        type=int,
+        default=32,
+        help='''maximum width of the integer values'''
+    )
+    parser.add_argument(
+        '--width-def',
+        choices=('signed', 'unsigned'),
+        default='signed',
+        help='''how the width is calculated'''
+    )
+    parser.add_argument(
+        'file',
+        help='''csv file with integer data set'''
+    )
+
+    return parser.parse_args()
+
+
+def main():
+    args = get_args()
+
+    data = read_data(args.file)
+
+    width = get_data_width(data, width_def=args.width_def)
 
     print(width)
 
