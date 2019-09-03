845eb459714853a9343de249245d17d1f77650b2
Miguel Pinho <miguel.m.pinho@tecnico.ulisboa.pt>
2019-09-03 12:42:18 +0100

    plots: Get width_class stats.

--- util/analysis/get_stats.sh
+++ util/analysis/get_stats.sh
@@ -4,5 +4,7 @@ find . -name 'stats.txt' | parallel 'awk -f clean_stats.awk {} > {//}/roi_stats.
 find . -name 'roi_stats.txt' > stat_files.txt
 parallel 'awk -f stat.awk -v stat="{2}" {1} | csvtool transpose - | csvcut -C samples,mean,stdev,underflows,overflows,min_value,max_value,total > {1//}/{2}.csv' :::: stat_files.txt :::: stat_list.txt
 parallel 'awk -f stat_vector.awk -v stat="{2}" {1} | ./csvpivot.py | csvcut -C samples,mean,stdev,underflows,overflows,min_value,max_value,total > {1//}/{2}.csv' :::: stat_files.txt :::: stat_vector_list.txt
+parallel 'awk -f stat.awk -v stat="widthClass" {1} | csvtool transpose - | csvcut -C total > {1//}/width_class.csv' :::: stat_files.txt
 parallel './stats_concat.py {1} {2}.csv' ::: fuse no-fuse :::: stat_list.txt stat_vector_list.txt
+parallel './stats_concat.py {1} {2}.csv' ::: fuse no-fuse ::: 'width_class'
 
