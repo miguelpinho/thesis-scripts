e7a43e98b0b15bdaa6c5e59b8b377b4ba5eff95c
Miguel Pinho <miguel.m.pinho@tecnico.ulisboa.pt>
2019-09-19 22:43:30 +0100

    stats: Improve stat parsing.

--- util/analysis/csvpivot.py
+++ util/analysis/csvpivot.py
@@ -3,11 +3,18 @@
 import sys
 import numpy as np
 import pandas as pd
+import argparse
 
+
+parser = argparse.ArgumentParser()
+parser.add_argument('col_name')
+args = parser.parse_args()
+
+coln = args.col_name
 df = pd.read_csv(sys.stdin, header=None,
-                 names=['fu', 'param', 'value'],
-                 dtype={'fu': np.str, 'param': np.str, 'value': np.str})
+                 names=[coln, 'param', 'value'],
+                 dtype={coln: np.str, 'param': np.str, 'value': np.str})
 
-df = df.pivot(index='fu', columns='param', values='value')
+df = df.pivot(index=coln, columns='param', values='value')
 
 df.to_csv(sys.stdout)
--- util/analysis/get_stats.sh
+++ util/analysis/get_stats.sh
@@ -4,8 +4,8 @@ find . -name 'stats.txt' | parallel 'awk -f clean_stats.awk {} > {//}/roi_stats.
 find . -name 'stats.txt' | parallel 'awk -f time.awk {} | csvtool transpose - > {//}/time.csv'
 find . -name 'roi_stats.txt' > stat_files.txt
 parallel 'awk -f stat.awk -v stat="{2}" {1} | csvtool transpose - | csvcut -C samples,mean,stdev,underflows,overflows,min_value,max_value,total > {1//}/{2}.csv' :::: stat_files.txt :::: stat_list.txt
-parallel 'awk -f stat_vector.awk -v stat="{2}" {1} | ./csvpivot.py | csvcut -C samples,mean,stdev,underflows,overflows,min_value,max_value,total > {1//}/{2}.csv' :::: stat_files.txt :::: stat_vector_list.txt
-parallel 'awk -f stat_by_class.awk -v stat="{2}" {1} | ./csvpivot.py | csvcut -C samples,mean,stdev,underflows,overflows,min_value,max_value,total > {1//}/{2}.csv' :::: stat_files.txt :::: stat_by_class_list.txt
+parallel 'awk -f stat_vector.awk -v stat="{2}" {1} | ./csvpivot.py "fu" | csvcut -C samples,mean,stdev,underflows,overflows,min_value,max_value,total > {1//}/{2}.csv' :::: stat_files.txt :::: stat_vector_list.txt
+parallel 'awk -f stat_by_class.awk -v stat="{2}" {1} | ./csvpivot.py "class" | csvcut -C samples,mean,stdev,underflows,overflows,min,max,total > {1//}/{2}.csv' :::: stat_files.txt :::: stat_by_class_list.txt
 parallel 'awk -f stat.awk -v stat="widthClass" {1} | csvtool transpose - | csvcut -C total > {1//}/width_class.csv' :::: stat_files.txt
 parallel './stats_concat.py {1} {2}.csv' :::: folders.txt ::: 'time'
 parallel './stats_concat.py {1} {2}.csv' :::: folders.txt :::: stat_list.txt stat_vector_list.txt
