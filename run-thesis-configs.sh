034cbc8a2f6fb54878fbb58a5ec92b8164bd5409
Miguel Pinho <miguel.m.pinho@tecnico.ulisboa.pt>
2019-10-21 20:10:41 +0100

    Add script to run all fs configs and benchmarks.

---
+++ run-thesis-configs.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+source .env
+
+echo "Running gem5 build in $GEM5_PATH"
+
+echo "Backup $GEM5_PATH/configs/common/cores/arm/O3_ARM_v7a.py"
+mv $GEM5_PATH/configs/common/cores/arm/O3_ARM_v7a.py \
+	$GEM5_PATH/configs/common/cores/arm/O3_ARM_v7a.py.backup
+
+declare -A config
+config[NF_4]="O3_ARM_v7a_nofuse_4fu.py"
+config[NF_2]="O3_ARM_v7a_nofuse_2fu.py"
+config[NF_1]="O3_ARM_v7a_nofuse_1fu.py"
+config[F_4]="O3_ARM_v7a_fuse_4fu.py"
+config[F_2]="O3_ARM_v7a_fuse_2fu.py"
+config[F_1]="O3_ARM_v7a_fuse_1fu.py"
+
+for cfg in "${!config[@]}"; do
+	printf "\nRun %s config:\n" $cfg
+	cp ./o3-configs/${config[$cfg]} $GEM5_PATH/configs/common/cores/arm/O3_ARM_v7a.py
+	./rungem5-fs.py --runs=2 --run-tag=$cfg scriptset ./sim-scripts/thesis-benchmarks.txt
+done
+
+printf "\nRestore %s/configs/common/cores/arm/O3_ARM_v7a.py" $GEM5_PATH
+mv $GEM5_PATH/configs/common/cores/arm/O3_ARM_v7a.py.backup \
+	$GEM5_PATH/configs/common/cores/arm/O3_ARM_v7a.py
