44f8427cb829005e864d843a565e09a9420253e7
Miguel Pinho <miguel.m.pinho@tecnico.ulisboa.pt>
2019-12-07 12:48:45 +0000

    util: Remove old scripts.

--- gem5/arm_fs_integerNN-checkpoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-SCRIPT_PATH=../sim-scripts/apps
-BENCHMARK_LIST=$SCRIPT_PATH/all.txt
-OUT_DIR=apps-ckpoints
-
-parallel --bar --max-procs $MACHINE_MAX_JOBS \
-    ../util/run_fs_1cpu_atomic.sh $SCRIPT_PATH/{} $OUT_DIR/{.} :::: $BENCHMARK_LIST
--- gem5/arm_fs_integerNN-trace.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-SCRIPT_PATH=../sim-scripts/apps
-BENCHMARK_LIST=$SCRIPT_PATH/all.txt
-OUT_DIR=apps-trace
-
-parallel --bar --max-procs $MACHINE_MAX_JOBS \
-    ../util/run_fs_1cpu_trace.sh $SCRIPT_PATH/{} $OUT_DIR/{.} :::: $BENCHMARK_LIST
