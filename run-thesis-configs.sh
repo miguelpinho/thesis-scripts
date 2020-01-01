96ca8b0a654a3db806ebe7b15b5bf94e1c678232
Miguel Pinho <miguel.m.pinho@tecnico.ulisboa.pt>
2020-01-01 15:18:15 +0000

    o3-configs: Refactor.

--- run-thesis-configs.sh
+++ run-thesis-configs.sh
@@ -9,12 +9,12 @@ mv $GEM5_PATH/configs/common/cores/arm/O3_ARM_v7a.py \
 	$GEM5_PATH/configs/common/cores/arm/O3_ARM_v7a.py.backup
 
 declare -A config
-config[NF_4]="O3_ARM_v7a_nofuse_4fu.py"
-config[F_4]="O3_ARM_v7a_fuse_4fu.py"
-config[NF_2]="O3_ARM_v7a_nofuse_2fu.py"
-config[F_2]="O3_ARM_v7a_fuse_2fu.py"
-config[NF_1]="O3_ARM_v7a_nofuse_1fu.py"
-config[F_1]="O3_ARM_v7a_fuse_1fu.py"
+config[HP3_NF]="highp_nofuse_3fu.py"
+config[HP2_NF]="highp_nofuse_2fu.py"
+config[HP1_NF]="highp_nofuse_1fu.py"
+config[HP3_F]="highp_fuse_3fu.py"
+config[HP2_F]="highp_fuse_2fu.py"
+config[HP1_F]="highp_fuse_1fu.py"
 config[A57_NF]="cortex_A57_nofuse.py"
 config[A57_F]="cortex_A57_fuse.py"
 config[A57_FP]="cortex_A57_fuse_penalty.py"
@@ -22,7 +22,7 @@ config[A57_FP]="cortex_A57_fuse_penalty.py"
 for cfg in "${!config[@]}"; do
 	printf "\nRun %s config:\n" $cfg
 	cp ./o3-configs/${config[$cfg]} $GEM5_PATH/configs/common/cores/arm/O3_ARM_v7a.py
-	./rungem5-fs.py --runs=1 --run-tag=$cfg scriptset ./sim-scripts/kernels_fast.txt
+	./rungem5-fs.py --runs=1 --run-tag=$cfg scriptset ./sim-scripts/thesis-benchmarks.txt
 done
 
 printf "\nRestore %s/configs/common/cores/arm/O3_ARM_v7a.py" $GEM5_PATH
