b1c47c2b750f01e57460df42ecd68fce0873e9e5
Miguel Pinho <miguel.m.pinho@tecnico.ulisboa.pt>
2020-02-13 18:04:48 +0000

    sim-scripts: Add ParVec run scripts.

---
+++ sim-scripts/parvec/streamcluster_simsmall_1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+BENCH_DIR="/root/benchmarks"
+PARSEC_DIR="$BENCH_DIR/parvec"
+cd $PARSEC_DIR
+pwd
+
+source ./env.sh
+parsecmgmt -a run -p parsec.simd.streamcluster -c gcc-hooks -i simsmall -n 1
+m5 exit
