6975bd3b9c732e1a8ba9d29721a9beef1ab0d85d
Miguel Pinho <miguel.m.pinho@tecnico.ulisboa.pt>
2020-03-06 11:10:47 +0000

    Add mcpat templates to emulate partial clock gating.

--- util/analysis/get_stats.sh
+++ util/analysis/get_stats.sh
@@ -12,9 +12,12 @@ find $1 -name 'roi.txt' | parallel --bar "awk -f $DIR/stat_vector.awk -v stat='{
 find $1 -name 'roi.txt' | parallel --bar "awk -f $DIR/stat_by_class.awk -v stat='{2}' {1} | $DIR/csvpivot.py 'class' > {1//}/{2}.csv" :::: - ::: statVectorInstTotalWidthByClass
 find $1 -name 'roi.txt' | parallel --bar "awk -f $DIR/width.awk {} > {//}/width.csv"
 find $1 -name 'roi.txt' | parallel --bar "$DIR/../mcpat/GEM5ToMcPAT.py --quiet --out={//}/mcpat.xml {} {//}/config.json {//}/../../template.xml"
+find $1 -name 'roi.txt' | parallel --bar "$DIR/../mcpat/GEM5ToMcPAT.py --quiet --out={//}/mcpat_clkgated.xml {} {//}/config.json {//}/../../template-clkgated.xml"
 if [[ $MCPAT ]]; then
     find $1 -name 'mcpat.xml' | parallel --bar "$MCPAT -infile {} -print_level 5 -opt_for_clk 0 > {//}/power.txt"
+    find $1 -name 'mcpat_clkgated.xml' | parallel --bar "$MCPAT -infile {} -print_level 5 -opt_for_clk 0 > {//}/power_clkgated.txt"
 else
     echo "No path for MCPAT provided."
 fi
 find $1 -name 'power.txt' | parallel --bar "$DIR/../mcpat/mcpat_tocsv.py {} > {.}.csv"
+find $1 -name 'power_clkgated.txt' | parallel --bar "$DIR/../mcpat/mcpat_tocsv.py {} > {.}.csv"
